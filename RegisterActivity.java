e54662e012b06b79be6526a1b532eb06bafb0e2b
park086357 <32133156+park086357@users.noreply.github.com>
2018-05-31 02:08:42 +0900

    Create RegisterActivity.java

---
+++ RegisterActivity.java
@@ -0,0 +1,103 @@
+package org.targetyou.targetyou;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextId;
+    private EditText editTextPw;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("                         SIGN UP");
+        setContentView(R.layout.activity_register);
+
+        editTextId = findViewById(R.id.new_id);
+        editTextPw = findViewById(R.id.new_pw);
+    }
+
+    public void insert(View view)
+    {
+        String Id = editTextId.getText().toString();
+        String Pw = editTextPw.getText().toString();
+
+        insertToDatabase(Id, Pw);
+
+        intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("email", Id);
+        intent.putExtra("password", Pw);
+    }
+
+    private void insertToDatabase(String Id, String Pw) {
+        class InsertData extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(RegisterActivity.this, "Please Wait", null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+
+                startActivity(intent);
+            }
+            @Override
+            protected String doInBackground(String... params) {
+
+                try {
+                    String Id = (String) params[0];
+                    String Pw = (String) params[1];
+
+                    String link = "phpURL";
+                    String data = URLEncoder.encode("Id", "UTF-8") + "=" + URLEncoder.encode(Id, "UTF-8");
+                    data += "&" + URLEncoder.encode("Pw", "UTF-8") + "=" + URLEncoder.encode(Pw, "UTF-8");
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                } catch (Exception e) {
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+        }
+        InsertData task = new InsertData();
+        task.execute(Id, Pw);
+    }
+}
