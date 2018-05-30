9389b3460f497ac7561a044106f464663dd7569c
park086357 <32133156+park086357@users.noreply.github.com>
2018-05-31 01:53:28 +0900

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,23 @@
+<?php
+$con=mysqli_connect(localhost, "ID", "password", "DBname");
+
+mysqli_set_charset($con, "utf8");
+
+if(mysqli_connect_errno($con)) {
+	echo “error”.mysqli_connect_error();
+}
+
+$userid = $_POST['Id'];
+$userpw = $_POST['Pw'];
+
+$result = mysqli_query($con,"insert into USER(USER_EMAIL,USER_PASS) values ('$userid', '$userpw')");
+
+if ($result) {
+  echo 'success';
+}
+else {
+  echo 'failure';
+}
+
+mysqli_close($con);
+?>
